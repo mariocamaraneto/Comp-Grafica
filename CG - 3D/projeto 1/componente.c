d2af2d4df477e7b4ca0a6a08add645128a6e1b5e
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-07-01 15:56:40 -0300

    Desenha botões na frente do gabinete e luz do HD

--- CG - 3D/projeto 1/componente.c
+++ CG - 3D/projeto 1/componente.c
@@ -2,13 +2,48 @@
 #include <GL/glut.h>
 #include "componente.h"
 
+int luzHDligada=0;
+
 void gabinete(float posx, float posy, float posz, float tamanho){
 	//função responsavel por contruir gabinete
 	//faz uma caixa com as seguintes proporções 50x60x30
 
-	caixaSemTampa(posx,posy,posz, 50*tamanho, 60*tamanho, 20*tamanho);
+	double tamRealX=50, tamRealY=60, tamRealZ=20;
+	double tamRealBotaoLiga = 1.5, tamRealBotaoReset = 0.8, tamRealLuzHD = 0.4;
 
-	GLUquadric* quad = gluNewQuadric();
+	caixaSemTampa(posx,posy,posz, tamRealX*tamanho, tamRealY*tamanho, tamRealZ*tamanho);
+
+	//faz a caracterização da frente do gabinete
+	glPushMatrix();
+		//prepara ambiente para edição
+		GLUquadric* quad = gluNewQuadric();
+		glTranslated( (posx+(tamRealX/2)*tamanho)+0.001,  0.0, 0.0);
+		glRotatef(90, 0.0f, 1.0f, 0.0f);
+
+		//desenha o botão de ligar e reset
+		glPushMatrix();
+			//desenha botão de liga
+			glTranslated(0.0, -7.0*tamanho, 0.0);
+			glColor3f(0.90f, 0.90f, 0.90f);
+			gluDisk( quad,  0,  tamRealBotaoLiga,  30,  1);
+
+			//desenha o botão de reset
+			glTranslated( 0.0, -(3.0*tamanho+tamRealBotaoLiga), 0.0);
+			gluDisk( quad,  0,  tamRealBotaoReset,  30,  1);
+		glPopMatrix();
+
+		glPushMatrix();
+			glTranslated( 0.0, -15*tamanho, 0.0);
+			if(luzHDligada)
+				glColor3f(1.0f, 0.0f, 0.0f);
+			else
+				glColor3f(0.30f, 0.30f, 0.30f);
+			gluDisk( quad,  0,  tamRealLuzHD,  30,  1);
+			glTranslated(0.0, 0.0, -0.5);
+			gluCylinder(quad,  tamRealLuzHD,  tamRealLuzHD,  1.0,  30,  10);
+		glPopMatrix();
+
+	glPopMatrix();
 }
 
 void memoriaRAM(float posx, float posy, float posz, float escala){
@@ -115,7 +150,7 @@ void caixaSemTampa(float posx, float posy, float posz, float tamx, float tamy, f
 	tamz = tamz/2;
 
 
-	glColor3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 0.0f);
 	//face1 (frontal)
 	/*
 	glBegin(GL_POLYGON);
