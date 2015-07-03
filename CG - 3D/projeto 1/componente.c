8ded9a9de1059ef1b813b54d6fec6d98f321f924
Mario Camara Neto <mariocamaraneto@gmail.com>
2015-07-03 18:53:04 -0300

    Desenha drive CD, Fonte, Luz de HD e de ligado e linhas de perspectiva Dentro da função gabinete foi inserido o código que desenha o drive de CD a Fonte e a luzes de uso de HD e de ligado e desligo do computador. As linhas de perspectiva são os contornas nas formas caixa para delimitar os vertices.

--- CG - 3D/projeto 1/componente.c
+++ CG - 3D/projeto 1/componente.c
@@ -2,20 +2,57 @@
 #include <GL/glut.h>
 #include "componente.h"
 
-int luzHDligada=0;
+int luzHDligada=0, luzPCLigada=1;
 
 void gabinete(float posx, float posy, float posz, float tamanho){
 	//função responsavel por contruir gabinete
 	//faz uma caixa com as seguintes proporções 50x60x30
 
 	double tamRealX=50, tamRealY=60, tamRealZ=20;
-	double tamRealBotaoLiga = 1.5, tamRealBotaoReset = 0.8, tamRealLuzHD = 0.4;
+	double tamRealCDX=19, tamRealCDY=4.5,tamRealCDZ=16;
+	double tamRealFonteX=14, tamRealFonteY=8.5, tamRealFonteZ=15;
+	double tamRealBotaoLiga = 1.5, tamRealBotaoReset = 0.8, tamRealLuz = 0.3;
+
 
 	caixaSemTampa(posx,posy,posz, tamRealX*tamanho, tamRealY*tamanho, tamRealZ*tamanho);
+
+	//Desenha Drive de CD
+		glColor3f(0.70f, 0.70f, 0.70f);
+		double CDx, CDy, CDz;
+		CDx=posx+(tamRealX*tamanho/2)-(tamRealCDX*tamanho/2)+0.05*tamanho;
+		CDy=posy+(tamRealY*tamanho/2)-(tamRealCDY*tamanho/2)-2*tamanho;
+		CDz=posz+(tamRealZ*tamanho/2)-(tamRealCDZ*tamanho/2)+((tamRealCDZ-tamRealZ)*tamanho/2);
+		caixa(CDx,CDy,CDz, tamRealCDX*tamanho, tamRealCDY*tamanho, tamRealCDZ*tamanho);
+
+	//Desenha Fonte
+		glColor3f(0.40f, 0.40f, 0.40f);
+		double fonteX, fonteY, fonteZ;
+		fonteX=posx-((tamRealX*tamanho/2)-(tamRealFonteX*tamanho/2)+0.05*tamanho);
+		fonteY=posy+((tamRealY*tamanho/2)-(tamRealFonteY*tamanho/2)-2*tamanho);
+		fonteZ=posz-((tamRealZ*tamanho/2)-(tamRealFonteZ*tamanho/2)+((tamRealFonteZ-tamRealZ)*tamanho/2));
+		caixa(fonteX,fonteY,fonteZ, tamRealFonteX*tamanho, tamRealFonteY*tamanho, tamRealFonteZ*tamanho);
+
+		glPushMatrix();
+			GLUquadric* quadis = gluNewQuadric();
+			glTranslated(fonteX-(tamRealFonteX*tamanho/2),  fonteY, fonteZ+4*tamanho);
+			glRotatef(-90, 0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 0.0f);
+			gluCylinder(quadis,  3*tamanho,  2.5*tamanho,  0.02,  30,  10);
+			gluCylinder(quadis,  2*tamanho,  1.5*tamanho,  0.02,  30,  10);
+			gluCylinder(quadis,  1*tamanho,  0.5*tamanho,  0.02,  30,  10);
+			glTranslated(-9.0*tamanho, 0, 0.01);
+			glBegin(GL_POLYGON);
+				glVertex3f(0, 0, 0);
+				glVertex3f(0, 2, 0);
+				glVertex3f(3, 2, 0);
+				glVertex3f(3, 0, 0);
+			glEnd();
+		glPopMatrix();
 
 	//faz a caracterização da frente do gabinete
 	glPushMatrix();
 		//prepara ambiente para edição
+		//deixa alinhado no centro da tampa da frente
 		GLUquadric* quad = gluNewQuadric();
 		glTranslated( (posx+(tamRealX/2)*tamanho)+0.001,  0.0, 0.0);
 		glRotatef(90, 0.0f, 1.0f, 0.0f);
@@ -32,15 +69,28 @@ void gabinete(float posx, float posy, float posz, float tamanho){
 			gluDisk( quad,  0,  tamRealBotaoReset,  30,  1);
 		glPopMatrix();
 
+		//Desenha led do HD
 		glPushMatrix();
-			glTranslated( 0.0, -15*tamanho, 0.0);
+			glTranslated( 0.0, -17*tamanho, 0.0);
 			if(luzHDligada)
 				glColor3f(1.0f, 0.0f, 0.0f);
 			else
 				glColor3f(0.30f, 0.30f, 0.30f);
-			gluDisk( quad,  0,  tamRealLuzHD,  30,  1);
+			gluDisk( quad,  0,  tamRealLuz,  30,  1);
 			glTranslated(0.0, 0.0, -0.5);
-			gluCylinder(quad,  tamRealLuzHD,  tamRealLuzHD,  1.0,  30,  10);
+			gluCylinder(quad,  tamRealLuz,  tamRealLuz,  1.0,  30,  10);
+		glPopMatrix();
+
+		//Desenha led do PC de ligado
+		glPushMatrix();
+			glTranslated( 0.0, -15*tamanho, 0.0);
+			if(luzPCLigada)
+				glColor3f(0.20f, 1.0f, 0.70f);
+			else
+				glColor3f(0.30f, 0.30f, 0.30f);
+			gluDisk( quad,  0,  tamRealLuz,  30,  1);
+			glTranslated(0.0, 0.0, -0.5);
+			gluCylinder(quad,  tamRealLuz,  tamRealLuz,  1.0,  30,  10);
 		glPopMatrix();
 
 	glPopMatrix();
@@ -59,9 +109,10 @@ void memoriaRAM(float posx, float posy, float posz, float escala){
 	caixa(posx, posy, posz, tamRealPenteX*escala, tamRealPenteY*escala, tamRealPenteZ*escala);
 
 	//desenha modulos de memoria
-	glColor3f(0.0f, 0.0f, 0.0f);
+
 	modPosY = (-tamRealPenteY*escala/2)+distanciaModBorda*escala;
 	for(int i=0; i<8; i++){
+		glColor3f(0.0f, 0.0f, 0.0f);
 		caixa(posx, modPosY, posz, tamRealModX*escala, tamRealModY*escala, tamRealModZ*escala);
 		if(i==3)
 			modPosY += distanciaMod*escala + (tamRealModY*escala);
@@ -107,8 +158,6 @@ void caixa(float posx, float posy, float posz, float tamx, float tamy, float tam
 			glVertex3f(posx-tamx, posy-tamy , posz-tamz);
 			glVertex3f(posx+tamx, posy-tamy , posz-tamz);
 	glEnd();
-
-
 	//face3 (superior)
 	glBegin(GL_POLYGON);
 			glVertex3f(posx+tamx, posy+tamy , posz+tamz);
@@ -123,8 +172,6 @@ void caixa(float posx, float posy, float posz, float tamx, float tamy, float tam
 			glVertex3f(posx-tamx, posy-tamy , posz-tamz);
 			glVertex3f(posx+tamx, posy-tamy , posz-tamz);
 	glEnd();
-
-
 	//face5 - (laterais)
 	glBegin(GL_POLYGON);
 			glVertex3f(posx+tamx, posy+tamy , posz+tamz);
@@ -134,10 +181,51 @@ void caixa(float posx, float posy, float posz, float tamx, float tamy, float tam
 	glEnd();
 	//face6 - oposta a 5
 	glBegin(GL_POLYGON);
+		glVertex3f(posx-tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz+tamz);
+	glEnd();
+
+
+	//Desenha vertices para dar noção de profundidade
+	glColor3f(0.30f, 0.30f, 0.30f);
+	glLineWidth(1.5);
+	glBegin(GL_LINE_LOOP);
+			glVertex3f(posx+tamx, posy+tamy , posz+tamz);
 			glVertex3f(posx-tamx, posy+tamy , posz+tamz);
-			glVertex3f(posx-tamx, posy+tamy , posz-tamz);
-			glVertex3f(posx-tamx, posy-tamy , posz-tamz);
 			glVertex3f(posx-tamx, posy-tamy , posz+tamz);
+			glVertex3f(posx+tamx, posy-tamy , posz+tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz-tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy+tamy , posz-tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy-tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz-tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx+tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz+tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx-tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz+tamz);
 	glEnd();
 }
 
@@ -200,5 +288,46 @@ void caixaSemTampa(float posx, float posy, float posz, float tamx, float tamy, f
 			glVertex3f(posx-tamx, posy+tamy , posz-tamz);
 			glVertex3f(posx-tamx, posy-tamy , posz-tamz);
 			glVertex3f(posx-tamx, posy-tamy , posz+tamz);
+	glEnd();
+
+
+	//Desenha vertices para dar noção de profundidade
+	glColor3f(0.30f, 0.30f, 0.30f);
+	glLineWidth(2.0);
+	glBegin(GL_LINE_LOOP);
+			glVertex3f(posx+tamx, posy+tamy , posz+tamz);
+			glVertex3f(posx-tamx, posy+tamy , posz+tamz);
+			glVertex3f(posx-tamx, posy-tamy , posz+tamz);
+			glVertex3f(posx+tamx, posy-tamy , posz+tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz-tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy+tamy , posz-tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy-tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz-tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx+tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx+tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx+tamx, posy-tamy , posz+tamz);
+	glEnd();
+	glBegin(GL_LINE_LOOP);
+		glVertex3f(posx-tamx, posy+tamy , posz+tamz);
+		glVertex3f(posx-tamx, posy+tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz-tamz);
+		glVertex3f(posx-tamx, posy-tamy , posz+tamz);
 	glEnd();
 }
